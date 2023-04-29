d850f9b7b1023510ff4738b3e96a9434121146f9
AClena <alcaaaa@seznam.cz>
2023-04-29 17:25:40 +0200

    Druhý pokus

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
+const EmailItem = (props) => {
+    const { unread, sender, subject, time } = props;
+
+    return `<div class="email">
+        <div class="email__head">
+            <button class="email__icon ${unread ? "email__icon--closed" : "email__icon--opened"}"></button>
+            <div class="email__info">
+                <div class="email__sender">${sender.name}</div>
+                <div class="email__subject">${subject}</div>
+            </div>
+            <div class="email__time">${time}</div>
+        </div>
+        <div class="email__body"></div>
+    </div>`
+}
+
 const displayEmails = ((emails) => {
     const read = document.querySelector('#read')
     const unread = document.querySelector('#unread')
     
-    unread.innerHTML += emails.map((email) => {
-        if (email.unread) {
-            return `<div class="email">
-                <div class="email__head">
-                    <button class="email__icon email__icon--closed"></button>
-                    <div class="email__info">
-                        <div class="email__sender">${email.sender.name}</div>
-                        <div class="email__subject">${email.subject}</div>
-                    </div>
-                    <div class="email__time">${email.time}</div>
-                </div>
-                <div class="email__body"></div>
-            </div>`
-        }
-    }).join(' ')
+    unread.innerHTML += emails
+        .filter(email => email.unread)
+        .map(email => EmailItem(email))
+        .join(' ')
 
-    read.innerHTML += emails.map((email) => {
-        if (!email.unread) {
-            return `<div class="email">
-                <div class="email__head">
-                    <button class="email__icon email__icon--opened"></button>
-                    <div class="email__info">
-                        <div class="email__sender">${email.sender.name}</div>
-                        <div class="email__subject">${email.subject}</div>
-                    </div>
-                    <div class="email__time">${email.time}</div>
-                </div>
-                <div class="email__body"></div>
-            </div>`
-        }
-    }).join(' ')
+    read.innerHTML += emails
+        .filter(email => !email.unread)
+        .map(email => EmailItem(email))
+        .join(' ')
 })
 
 fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails')
